a358b21e199b82bb5e0c7f2d1aafd72b844de72c
Optera <yousei.kyoko+github@gmail.com>
2018-03-30 15:07:03 +0200

    support for Bob v0.16.x #23 (#24)

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,6 +1,4 @@
-function make_loader_item(name,order)
-	local subgroup="belt"
-	if name == "green-loader" or name == "purple-loader" then subgroup="bob-belt" end
+function make_loader_item(name, subgroup, order)
 	return{
 		type="item",
 		name=name,
@@ -14,20 +12,18 @@ function make_loader_item(name,order)
 	}
 end
 
-data:extend({
-  make_loader_item("loader","d-a"),
-  make_loader_item("fast-loader","d-b"),
-  make_loader_item("express-loader","d-c"),
-})
-
-if data.raw.item["green-transport-belt"] then
+if data.raw.technology["bob-logistics-4"] then
   data:extend({
-    make_loader_item("green-loader","d-d"),
+    make_loader_item("loader","bob-logistic-tier-1","d-a"),
+    make_loader_item("fast-loader","bob-logistic-tier-2","d-b"),
+    make_loader_item("express-loader","bob-logistic-tier-3","d-c"),  
+    make_loader_item("purple-loader","bob-logistic-tier-4","d-d"),
+    make_loader_item("green-loader","bob-logistic-tier-5","d-f"),    
   })
-end
-
-if data.raw.item["purple-transport-belt"] then
-  data:extend({  
-    make_loader_item("purple-loader","d-e"),
+else
+  data:extend({
+    make_loader_item("loader","belt","d-a"),
+    make_loader_item("fast-loader","belt","d-b"),
+    make_loader_item("express-loader","belt","d-c"),
   })
 end
